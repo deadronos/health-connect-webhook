cfbeb8e8e89f7567af23a5ff0d2ed2f752a37f66
deadronos <iridanblub@hotmail.de>
2026-04-18 16:28:25 +0200

    chore: update Gradle and Kotlin plugin versions, add Compose plugin, and configure project code style

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.kotlinx.serialization)
+    alias(libs.plugins.kotlin.compose)
 }
 
 val appVersionMajor = 1
@@ -66,9 +67,6 @@ android {
     }
     buildFeatures {
         compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
     }
     packaging {
         resources {
